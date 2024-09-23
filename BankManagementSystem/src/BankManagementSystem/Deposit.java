90344eb2d5a0fa5382589c8c94acccdc0417a4f1
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-09-23 16:09:54 +0530

    Added component resizing

--- BankManagementSystem/src/BankManagementSystem/Deposit.java
+++ BankManagementSystem/src/BankManagementSystem/Deposit.java
@@ -2,8 +2,7 @@ package BankManagementSystem;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -12,37 +11,43 @@ import java.util.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+
 import javax.swing.JButton;
 
-public class Deposit extends JFrame implements ActionListener {
+public class Deposit extends ResizableATM implements ActionListener {
 
     String pin = null;
 
+    JLabel depositAmountLabel;
     JTextField amounTextField;
 
     JButton depositButton;
     JButton backButton;
 
     Deposit(String inPin) {
+        super("Depost Money");
 
         pin = inPin;
 
-        Common.InitializeJFrame(this, "Deposit Money", null, new Dimension(1550, 1080), JFrame.EXIT_ON_CLOSE, false,
-                new Point(0, 0));
+        initializeComponents();
+        setVisible(true);
+    }
 
-        JLabel backgroundImageLabel = Common.GetScaledImageWithLabel("icons/atm2.png", 1550, 830);
-        backgroundImageLabel.setBounds(0, 0, 1550, 830);
-        add(backgroundImageLabel);
+    private void initializeComponents() {
+        initializeLabelsAndFields();
+        initializeButtons();
+    }
 
-        JLabel depositAmountLabel = Common.CreateLabel("ENTER AMOUNT YOU WANT TO DEPOSIT", Color.WHITE,
-                Common.SystemBold16, new Rectangle(460, 180, 400, 35));
+    private void initializeLabelsAndFields() {
+        depositAmountLabel = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
+        depositAmountLabel.setForeground(Color.WHITE);
         backgroundImageLabel.add(depositAmountLabel);
 
-        amounTextField = Common.CreateTextField(Common.RalewayBold22, new Rectangle(460, 230, 320, 25));
+        amounTextField = new JTextField();
 
         // Add a KeyListener to only allow numeric input
         amounTextField.addKeyListener(new KeyAdapter() {
@@ -57,17 +62,16 @@ public class Deposit extends JFrame implements ActionListener {
         });
 
         backgroundImageLabel.add(amounTextField);
+    }
 
-        depositButton = Common.CreateButton("DEPOSIT", Common.RalewayBold18, Color.BLACK,
-                new Rectangle(700, 362, 150, 35), this);
+    private void initializeButtons() {
+        depositButton = new JButton("DEPOSIT");
+        Common.setButtonAttributes(depositButton, Color.BLACK, this);
         backgroundImageLabel.add(depositButton);
 
-        backButton = Common.CreateButton("BACK", Common.RalewayBold18, Color.BLACK,
-                new Rectangle(700, 406, 150, 35), this);
+        backButton = new JButton("BACK");
+        Common.setButtonAttributes(backButton, Color.BLACK, this);
         backgroundImageLabel.add(backButton);
-
-        setVisible(true);
-
     }
 
     @Override
@@ -124,6 +128,68 @@ public class Deposit extends JFrame implements ActionListener {
             }
             e.printStackTrace();
         } 
+    }
+
+    @Override
+    protected void handleResizing() {
+        super.handleResizing();
+
+        if(!isVisible()) return;
+
+        Dimension size = getSize();
+
+        updateLabels(size);
+        updateTextFields(size);
+        updateButtons(size);
+    }
+
+    private void updateLabels(Dimension size) {
+        Font scaledFont16 = Common.SystemBold16.deriveFont(size.width/1400.0f * 16);
+        depositAmountLabel.setFont(scaledFont16);
+
+        int x = (int) (size.width/ 3.4);
+        int y = (int) (size.height / 4.9);
+        int width = 400;
+        int height = 35;
+        depositAmountLabel.setBounds(x, y, width, height);
+    }
+
+    private void updateTextFields(Dimension size) {
+        Font scaledFont22 = Common.RalewayBold22.deriveFont(size.width/1400.0f * 22 );
+        amounTextField.setFont(scaledFont22);
+
+        int x = (int) (size.width / 3.4);
+        int y = (int) (size.height / 4);
+        int width = (int) (size.width / 4.84);
+        int height = (int) (size.width / 50);
+        amounTextField.setBounds(x, y, width, height);
+    }
+
+    private void updateButtons(Dimension size) {
+        updateDepositButtonLocation(size);
+        updateBackButtonLocation(size);
+    }
+
+    private void updateDepositButtonLocation(Dimension size) {
+        Font scaledFont18 = Common.RalewayBold18.deriveFont(size.width / 1400.0f * 18);
+        depositButton.setFont(scaledFont18);
+
+        int x = (int) (size.width / 2.25);
+        int y = (int) (size.height / 2.4);
+        int width = (int) (size.width / 10);
+        int height = (int) (size.height / 25);
+        depositButton.setBounds(x, y, width, height);
+    }
+
+    private void updateBackButtonLocation(Dimension size) {
+        Font scaledFont18 = Common.RalewayBold18.deriveFont(size.width / 1400.0f * 18);
+        backButton.setFont(scaledFont18);
+        
+        int x = (int) (size.width / 2.25);
+        int y = (int) (size.height / 2.1 );
+        int width = (int) (size.width / 10);
+        int height = (int) (size.height / 25);
+        backButton.setBounds(x, y, width, height);
     }
 
     public static void main(String[] args) {
