b634cecb2706bb8a5513ba87ea93028f5c67a74c
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-09-28 21:16:31 +0530

    Added function to verify if UID is already in use or not.

--- BankManagementSystem/src/BankManagementSystem/UniqueIDGenerator.java
+++ BankManagementSystem/src/BankManagementSystem/UniqueIDGenerator.java
@@ -1,26 +1,24 @@
 package BankManagementSystem;
 
 import java.security.SecureRandom;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UniqueIDGenerator {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final int UID_LENGTH = 6; // 56 billion combinations :O
     private static final SecureRandom random = new SecureRandom();
-    private static final Set<String> generatedUIDs = new HashSet<>();
 
 
     // find a way to check if generate id is already used or not
 
     public static String generateUID() {
-        String uid;
-        do {
-            uid = generateRandomUID();
-        } while (generatedUIDs.contains(uid)); 
 
-        generatedUIDs.add(uid);
-        return uid;
+        while(true){
+            String uid;
+            uid = generateRandomUID();
+            if(!isUIDExists(uid)) return uid;
+        }
     }
 
     private static String generateRandomUID() {
@@ -30,5 +28,29 @@ public class UniqueIDGenerator {
             uid.append(CHARACTERS.charAt(randomIndex));
         }
         return uid.toString();
+    }
+
+    private static boolean isUIDExists(String uid) {
+
+        try (MyCon con = new MyCon()) {
+            String query = "SELECT UID FROM login WHERE UID = ?";
+            
+            PreparedStatement preparedStatement = con.connection.prepareStatement(query);
+            preparedStatement.setString(1, uid);
+            
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    preparedStatement.close();
+                    return false;
+                }
+            }
+
+            preparedStatement.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
