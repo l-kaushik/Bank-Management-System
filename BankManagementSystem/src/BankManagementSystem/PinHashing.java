57e43afa9c162d9d55ec4f7dad1a63dea52a8fba
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-09-29 19:35:26 +0530

    Updated conversion functions

--- BankManagementSystem/src/BankManagementSystem/PinHashing.java
+++ BankManagementSystem/src/BankManagementSystem/PinHashing.java
@@ -32,8 +32,21 @@ public class PinHashing {
         }
     }
 
-    public static String byteToString(byte[] input) {
-        String str = new String(input, StandardCharsets.UTF_8);
-        return str;
+    public static String byteToString(byte[] byteArray) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : byteArray) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static byte[] stringToByte(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                 + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 }
