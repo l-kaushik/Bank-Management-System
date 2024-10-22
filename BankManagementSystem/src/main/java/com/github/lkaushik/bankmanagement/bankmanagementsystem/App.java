7a0f0f3bf246bc9fc5af76080925d2bb603c42ab
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-22 18:38:43 +0530

    Created left client menu Added buttons in controller class

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/App.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/App.java
@@ -1,7 +1,17 @@
 package com.github.lkaushik.bankmanagement.bankmanagementsystem;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
