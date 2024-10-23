fd9378818382ca95afe05cf76e9af519cf59cc3e
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-23 17:34:43 +0530

    Login window creation
    1. Added action event on login button.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/Controllers/LoginController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/Controllers/LoginController.java
@@ -1,15 +1,25 @@
 package com.github.lkaushik.bankmanagement.bankmanagementsystem.Controllers;
 
+import com.github.lkaushik.bankmanagement.bankmanagementsystem.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
     public TextField password_fld;
     public Button login_btn;
     public Label error_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(event -> Model.getInstance().getViewFactory().showClientWindow());
+    }
 }
