e0022a8c265e7050ef85b51f1da97ea0bed6daaf
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-23 17:32:55 +0530

    Singleton class for handling instances.
    1. Returns model instance and viewfactory.
    2. Prevents multiple creation or access to model instance.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/Models/Model.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/bankmanagementsystem/Models/Model.java
@@ -1,4 +1,23 @@
 package com.github.lkaushik.bankmanagement.bankmanagementsystem.Models;
 
+import com.github.lkaushik.bankmanagement.bankmanagementsystem.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    // synchronized make sure only 1 thread at a time can execute this method (Singleton pattern)
+    public static synchronized Model getInstance() {
+        if(model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
