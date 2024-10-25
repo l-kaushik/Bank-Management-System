9142c2d461a98593344b12a798b1ec5b9147cf09
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-25 18:00:40 +0530

    Added admin menu view
    1. fxml, controller and css.

---
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,10 @@
+package com.github.lkaushik.bankmanagement.Controllers.Admin;
+
+import javafx.scene.control.Button;
+
+public class AdminMenuController {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposit_btn;
+    public Button logout_btn;
+}
