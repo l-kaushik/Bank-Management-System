134c8cef61c15becf6923d8a317a0cba97a03474
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-26 13:18:16 +0530

    Added listeners and menu item setter.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/AdminMenuController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/AdminMenuController.java
@@ -1,10 +1,26 @@
 package com.github.lkaushik.bankmanagement.Controllers.Admin;
 
+import com.github.lkaushik.bankmanagement.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class AdminMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
     public Button create_client_btn;
     public Button clients_btn;
     public Button deposit_btn;
     public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        addListeners();
+    }
+
+    private void addListeners() {}
+
+    private void onCreateClient() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set("CreateClient");
+    }
 }
