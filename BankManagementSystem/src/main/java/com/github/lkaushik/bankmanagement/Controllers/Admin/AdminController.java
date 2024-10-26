f6ce4520b478e2de2c63e8b25e41ec7c772f8cf5
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-26 13:17:20 +0530

    Implemented initializable interface.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/AdminController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/AdminController.java
@@ -1,4 +1,19 @@
 package com.github.lkaushik.bankmanagement.Controllers.Admin;
 
-public class AdminController {
+import com.github.lkaushik.bankmanagement.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+    public BorderPane admin_parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().addListener((observable, oldVal, newVal) -> {
+            // Add switch statements
+        } );
+    }
 }
