fa3dd0093e428ee98e7c2cf41f25e0f496ec9e6e
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-27 16:43:09 +0530

    Created constructor to handle respective object.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientCellController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientCellController.java
@@ -1,5 +1,6 @@
 package com.github.lkaushik.bankmanagement.Controllers.Admin;
 
+import com.github.lkaushik.bankmanagement.Models.Client;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,6 +16,12 @@ public class ClientCellController implements Initializable {
     public Label sv_acc_lbl;
     public Label date_lbl;
     public Button delete_btn;
+
+    private final Client client;
+
+    public ClientCellController(Client client) {
+        this.client = client;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
