d027b0b623b9e0507c09c4a3f451b2d8602fbd94
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-10-27 19:17:56 +0530

    Created Client class.
    Added getters and constructor.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/Client.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/Client.java
@@ -1,4 +1,34 @@
 package com.github.lkaushik.bankmanagement.Models;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDate;
+
 public class Client {
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty payeeAddress;
+    private final ObjectProperty<Account> checkingAccount;
+    private final ObjectProperty<Account> savingsAccount;
+    private final ObjectProperty<LocalDate> dateCreated;
+
+    public Client(String fName, String lName, String pAddress, Account cAccount, Account sAccount, LocalDate date) {
+        this.firstName = new SimpleStringProperty(this, "First Name", fName);
+        this.lastName = new SimpleStringProperty(this, "Last Name", lName);
+        this.payeeAddress = new SimpleStringProperty(this, "Payee Address", pAddress);
+        this.checkingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
+        this.savingsAccount = new SimpleObjectProperty<>(this, "Saving Account", sAccount);
+        this.dateCreated = new SimpleObjectProperty<>(this, "Date Created", date);
+    }
+
+    // getters
+    public StringProperty firstNameProperty() {return firstName;}
+    public StringProperty lastNameProperty() {return lastName;}
+    public StringProperty payeeAddressProperty() {return payeeAddress;}
+    public ObjectProperty<Account> checkingAccountProperty() {return checkingAccount;}
+    public ObjectProperty<Account> savingAccountProperty() {return savingsAccount;}
+    public ObjectProperty<LocalDate> dateProperty() {return dateCreated;}
 }
