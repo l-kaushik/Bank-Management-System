c3d208cb9267b68f101c83e3242e492e134ac232
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-02 12:45:21 +0530

    Added methods to update labels.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Client/TransactionCellController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Client/TransactionCellController.java
@@ -1,11 +1,13 @@
 package com.github.lkaushik.bankmanagement.Controllers.Client;
 
+import com.github.lkaushik.bankmanagement.Models.Model;
 import com.github.lkaushik.bankmanagement.Models.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class TransactionCellController implements Initializable {
@@ -24,6 +26,24 @@ public class TransactionCellController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateLabels();
+    }
 
+    private void updateLabels() {
+        trans_date_lbl.setText(transaction.dateProperty().getValue().toString());
+        sender_lbl.setText(transaction.senderProperty().getValue());
+        receiver_lbl.setText(transaction.receiverProperty().getValue());
+        amount_lbl.setText(transaction.amountProperty().getValue().toString());
+
+        String payeeAddress = Model.getInstance().getClient().payeeAddressProperty().getValue();
+
+        if(Objects.equals(payeeAddress, receiver_lbl.getText())) {
+            in_icon.setVisible(true);
+            out_icon.setVisible(false);
+        }
+        else {
+            in_icon.setVisible(false);
+            out_icon.setVisible(true);
+        }
     }
 }
