d449f9058edc8ce04e629f3e875c7f251e44c1bf
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-02 12:46:23 +0530

    Added method of settings transaction cells to list view.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Client/TransactionsController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Client/TransactionsController.java
@@ -1,16 +1,24 @@
 package com.github.lkaushik.bankmanagement.Controllers.Client;
 
+import com.github.lkaushik.bankmanagement.Models.Model;
+import com.github.lkaushik.bankmanagement.Models.Transaction;
+import com.github.lkaushik.bankmanagement.Views.TransactionCellFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class TransactionsController implements Initializable {
-    public ListView transactions_listview;
+    public ListView<Transaction> transactions_listview;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        transactions_listview.setCellFactory(param -> new TransactionCellFactory());
+        ObservableList<Transaction> transactions = Model.getInstance().getClientTransactionData();
+        transactions_listview.setItems(transactions);
     }
 }
