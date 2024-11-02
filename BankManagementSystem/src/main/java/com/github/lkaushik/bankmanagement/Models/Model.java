b00640a09e39dd2cbd974bfb2e961eb17fb5f985
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-02 12:44:36 +0530

    Added transaction data handling.
    1. Added ObservableList to store all transaction data.
    2. Added function to retrieve it.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/Model.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/Model.java
@@ -3,6 +3,8 @@ package com.github.lkaushik.bankmanagement.Models;
 import com.github.lkaushik.bankmanagement.Views.AccountType;
 import com.github.lkaushik.bankmanagement.Views.ClientAccountType;
 import com.github.lkaushik.bankmanagement.Views.ViewFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
 import java.time.LocalDate;
@@ -15,6 +17,7 @@ public class Model {
     // Client Data Section
     private final Client client;
     private boolean clientLoginSuccessFlag;
+    private final ObservableList<Transaction> clientTransactionData;
     // Admin Data Section
 
     private Model() {
@@ -23,6 +26,7 @@ public class Model {
         // Client Data Section
         this.clientLoginSuccessFlag = false;
         this.client = new Client();
+        this.clientTransactionData = FXCollections.observableArrayList();
         // Admin Data Section
     }
 
@@ -39,6 +43,7 @@ public class Model {
     public DatabaseDriver getDatabaseDriver() {return databaseDriver;}
     public AccountType getLoginAccountType() {return loginAccountType;}
     public void setLoginAccountType(AccountType type) {this.loginAccountType = type;}
+    public ObservableList<Transaction> getClientTransactionData() {return clientTransactionData;}
 
     /*
     * Client Method Section
@@ -59,6 +64,7 @@ public class Model {
                 this.client.dateProperty().set(date);
                 this.client.savingAccountProperty().set(getAccountsData(pAddress, ClientAccountType.SAVINGS));
                 this.client.checkingAccountProperty().set(getAccountsData(pAddress, ClientAccountType.CHECKING));
+                this.clientTransactionData.addAll(getTransactionData(pAddress));
                 this.clientLoginSuccessFlag = true;
             }
         } catch (Exception e) {
@@ -91,5 +97,27 @@ public class Model {
         }
         return account;
     }
+
+    private ObservableList<Transaction> getTransactionData(String pAddress) {
+        ObservableList<Transaction> transactions = FXCollections.observableArrayList();
+        ResultSet resultSet = databaseDriver.getClientTransactionData(pAddress);
+        try{
+            while (resultSet.next()) {
+                String sender = resultSet.getString("Sender");
+                String receiver = resultSet.getString("Receiver");
+                double amount = resultSet.getDouble("Amount");
+                String[] dateParts = resultSet.getString("Date").split("-");
+                LocalDate date = LocalDate.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[2]));
+                String message = resultSet.getString("Message");
+
+                transactions.add(new Transaction(sender, receiver, amount, date, message));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return transactions;
+    }
+
+
 
 }
