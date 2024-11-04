5db3981de3516a40f34d2c080c587c583c996247
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-04 20:47:05 +0530

    Add login functionality for Client and Admin users
    - Implement acc_selector event handler to update UI label based on selected account type (Admin or Payee).
    - Add performClientLogin and performAdminLogin methods to handle login logic for Client and Admin users, respectively.
    - Add performCleanupAndDisplayError method to clear input fields and display error message on login failure.
    - Display appropriate windows on successful login and close the login stage.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/LoginController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/LoginController.java
@@ -3,6 +3,8 @@ package com.github.lkaushik.bankmanagement.Controllers;
 import com.github.lkaushik.bankmanagement.Models.Model;
 import com.github.lkaushik.bankmanagement.Views.AccountType;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -27,27 +29,58 @@ public class LoginController implements Initializable {
         acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
         acc_selector.valueProperty().addListener(observable -> Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
         login_btn.setOnAction(event ->onLogin());
+
+        acc_selector.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(acc_selector.getValue() == AccountType.ADMIN) {
+                    payee_address_lbl.setText("Admin Username:");
+                }
+                else {
+                    payee_address_lbl.setText("Payee Address:");
+                }
+            }
+        });
     }
 
     private void onLogin() {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
 
         if(Model.getInstance().getViewFactory().getLoginAccountType() == AccountType.CLIENT) {
-            // Evaluate Login Credentials
-            Model.getInstance().evaluateClientCred(payee_address_fld.getText(), password_fld.getText());
-            if(Model.getInstance().getClientLoginSuccessFlag()) {
-                Model.getInstance().getViewFactory().showClientWindow();
-                // Close the login stage
-                Model.getInstance().getViewFactory().closeStage(stage);
-            }
-            else {
-                payee_address_fld.setText("");
-                password_fld.setText("");
-                error_lbl.setText("No Such Login Credentials");
-            }
+            performClientLogin(stage);
         }
         else {
-            Model.getInstance().getViewFactory().showAdminWindow();
+            performAdminLogin(stage);
         }
+    }
+
+    private void performClientLogin(Stage stage) {
+        // Evaluate Login Credentials
+        Model.getInstance().evaluateClientCred(payee_address_fld.getText(), password_fld.getText());
+        if(Model.getInstance().getClientLoginSuccessFlag()) {
+            Model.getInstance().getViewFactory().showClientWindow();
+            // Close the login stage
+            Model.getInstance().getViewFactory().closeStage(stage);
+        }
+        else {
+            performCleanupAndDisplayError();
+        }
+    }
+
+    private void performAdminLogin(Stage stage) {
+        Model.getInstance().evaluateAdminCreds(payee_address_fld.getText(), password_fld.getText());
+        if(Model.getInstance().getAdminLoginSuccessFlag()) {
+            Model.getInstance().getViewFactory().showAdminWindow();
+            Model.getInstance().getViewFactory().closeStage(stage);
+        }
+        else {
+            performCleanupAndDisplayError();
+        }
+    }
+
+    private void performCleanupAndDisplayError() {
+        payee_address_fld.setText("");
+        password_fld.setText("");
+        error_lbl.setText("No Such Login Credentials");
     }
 }
