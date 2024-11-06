e2584475379af4d1b8d78250935616acb61fecc8
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-06 17:20:11 +0530

    Implemented listener for client deletion.
    - Update the list of clients, when deletion completed.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientsController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientsController.java
@@ -3,6 +3,7 @@ package com.github.lkaushik.bankmanagement.Controllers.Admin;
 import com.github.lkaushik.bankmanagement.Models.Client;
 import com.github.lkaushik.bankmanagement.Models.Model;
 import com.github.lkaushik.bankmanagement.Views.ClientCellFactory;
+import com.github.lkaushik.bankmanagement.Views.ClientDeletionListener;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -11,13 +12,24 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class ClientsController implements Initializable {
+public class ClientsController implements Initializable, ClientDeletionListener {
     public ListView<Client> clients_listview;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Model.getInstance().addClientDeletionListener(this);
+        updateList();
+    }
+
+    private void updateList() {
+        clients_listview.getItems().removeAll();
         clients_listview.setCellFactory(param -> new ClientCellFactory());
         List<Client> clients = Model.getInstance().getExistingClients().reversed();
         clients_listview.setItems(FXCollections.observableArrayList(clients));
+    }
+
+    @Override
+    public void onClientDeletionCompleted() {
+        updateList();
     }
 }
