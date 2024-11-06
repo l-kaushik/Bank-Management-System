ab453c3410a7360f11645a965e295f3ab017034c
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-06 16:38:52 +0530

    Removed unused import and console print statement.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientCellController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/Admin/ClientCellController.java
@@ -1,11 +1,16 @@
 package com.github.lkaushik.bankmanagement.Controllers.Admin;
 
+import com.github.lkaushik.bankmanagement.Models.AlertBoxCreator;
 import com.github.lkaushik.bankmanagement.Models.Client;
+import com.github.lkaushik.bankmanagement.Models.Model;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ClientCellController implements Initializable {
@@ -26,6 +31,7 @@ public class ClientCellController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         updateLabels();
+        delete_btn.setOnAction(event -> onDeleteButtonClicked());
     }
 
     private void updateLabels() {
@@ -36,4 +42,12 @@ public class ClientCellController implements Initializable {
         sv_acc_lbl.setText(client.savingAccountProperty().getValue().accountNumberProperty().getValue());
         date_lbl.setText(client.dateProperty().getValue().toString());
     }
+
+    private void onDeleteButtonClicked() {
+        Optional<ButtonType> result = AlertBoxCreator.createAlert(Alert.AlertType.CONFIRMATION, "Client removal",
+                "Are you sure you want to remove " + pAddress_lbl.getText() + "?\n This can't be undone.",true);
+       if(result.isPresent() && (result.get() == ButtonType.OK))
+            Model.getInstance().removeClient(pAddress_lbl.getText());
+    }
+
 }
