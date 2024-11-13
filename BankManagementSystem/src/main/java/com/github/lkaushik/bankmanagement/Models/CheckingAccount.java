54285912b4609e6e2e9c70727ec39d145ba17f5d
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-13 17:59:03 +0530

    Implemented default constructor for accounts.
    - Added for checking and savings account type.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/CheckingAccount.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/CheckingAccount.java
@@ -7,6 +7,10 @@ public class CheckingAccount extends Account{
     // The number of transactions a client is allowed to do per day.
     private final IntegerProperty transactionLimit;
 
+    public CheckingAccount() {
+        this("", "0000 0000 0000 0000", 0.0, 0);
+    }
+
     public CheckingAccount(String owner, String accountNumber, double balance, int tLimit) {
         super(owner, accountNumber, balance);
         this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", tLimit);
--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/SavingsAccount.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Models/SavingsAccount.java
@@ -7,6 +7,10 @@ public class SavingsAccount extends Account{
     // The withdrawal limit from the savings
     private final DoubleProperty withdrawalLimit;
 
+    public SavingsAccount() {
+        this("", "0000 0000 0000 0000", 0.0, 0.0);
+    }
+
     public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
         super(owner, accountNumber, balance);
         this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit", withdrawalLimit);
