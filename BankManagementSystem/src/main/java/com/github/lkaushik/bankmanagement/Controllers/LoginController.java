1d7b2d06b3f252a2669322fb8bc2e51bdbaef420
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-11-17 15:20:00 +0530

    Implemented password view toggle button.

--- BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/LoginController.java
+++ BankManagementSystem/src/main/java/com/github/lkaushik/bankmanagement/Controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.github.lkaushik.bankmanagement.Controllers;
 
 import com.github.lkaushik.bankmanagement.Models.Model;
 import com.github.lkaushik.bankmanagement.Views.AccountType;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -22,25 +24,43 @@ public class LoginController implements Initializable {
     public TextField password_fld;
     public Button login_btn;
     public Label error_lbl;
+    public TextField password_txt_fld;
+    public Button pass_toggle_btn;
+    public FontAwesomeIconView toggle_icon;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         acc_selector.setItems(FXCollections.observableArrayList(AccountType.CLIENT, AccountType.ADMIN));
         acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
         acc_selector.valueProperty().addListener(observable -> Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
-        login_btn.setOnAction(event ->onLogin());
 
-        acc_selector.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if(acc_selector.getValue() == AccountType.ADMIN) {
-                    payee_address_lbl.setText("Admin Username:");
-                }
-                else {
-                    payee_address_lbl.setText("Payee Address:");
-                }
-            }
-        });
+        login_btn.setOnAction(event -> onLogin());
+        acc_selector.setOnAction(_ -> onAccountSelector());
+        pass_toggle_btn.setOnAction(_ -> onPasswordToggle());
+    }
+
+    private void onPasswordToggle() {
+        if(Objects.equals(toggle_icon.getGlyphName(), "EYE_SLASH")) {
+            toggle_icon.setGlyphName("EYE");
+            password_txt_fld.setVisible(true);
+            password_fld.setVisible(false);
+            password_txt_fld.setText(password_fld.getText());
+        }
+        else {
+            toggle_icon.setGlyphName("EYE_SLASH");
+            password_txt_fld.setVisible(false);
+            password_fld.setVisible(true);
+            password_fld.setText(password_txt_fld.getText());
+        }
+    }
+
+    private void onAccountSelector() {
+        if(acc_selector.getValue() == AccountType.ADMIN) {
+            payee_address_lbl.setText("Admin Username:");
+        }
+        else {
+            payee_address_lbl.setText("Payee Address:");
+        }
     }
 
     private void onLogin() {
